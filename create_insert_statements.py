24f369dc9c5b2eba5ad203108168599850fc34eb
Johann Baydeer <75598081+chiefeno@users.noreply.github.com>
2024-03-07 14:16:28 +0100

    Update create_insert_statements.py

--- create_insert_statements.py
+++ create_insert_statements.py
@@ -1,5 +1,5 @@
 # CREATE TABLE STATEMENTS
-create_listing_table = (""" CREATE TABLE airbnb (
+create_listing_table = (""" CREATE TABLE listing (
                                                             id INTEGER,
                                                             name VARCHAR,
                                                             host_id INTEGER,
@@ -17,17 +17,31 @@ create_listing_table = (""" CREATE TABLE airbnb (
                                                             country VARCHAR,
                                                             coordinates VARCHAR,
                                                             full_location VARCHAR,
-                                                            companies JSON
                                                             );
                                                   
 """)
 
 
+create_company_table = (""" CREATE TABLE company (
+                                                            id INTEGER,
+                                                            address VARCHAR,
+                                                            city VARCHAR,
+                                                            zip INTEGER,
+                                                            created_at DATE
+                                                            );
+                                                  
+""")
 
 
 # INSERT STATEMENTS
-listing_table_insert = ("""INSERT INTO airbnb(id, name, host_id, neighbourhood, room_type, column_10, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365, updated_date, city, country, coordinates, full_location, companies)
-                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+listing_table_insert = ("""INSERT listing (id, name, host_id, neighbourhood, room_type, column_10, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365, updated_date, city, country, coordinates, full_location)
+                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                        ON CONFLICT
+                        DO NOTHING;
+""")
+
+company_table_insert = ("""INSERT listing (id, address, city, zip, created_at)
+                        VALUES(%s,%s,%s,%s,%s)
                         ON CONFLICT
                         DO NOTHING;
 """)
