695150e2ad835ec06ca2167032467e2e61bd89d9
Johann Baydeer <75598081+chiefeno@users.noreply.github.com>
2024-03-07 14:17:23 +0100

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,76 +1,53 @@
 import os
 import json
+import csv
 from datetime import datetime
 from db_config import DB_HOST, DB_NAME, DB_PORT
 import pandas as pd
 import psycopg2
-from create_insert_statements import create_listing_table, listing_table_insert
-
-
-# Read the first CSV file
-df1 = pd.read_csv('air_bnb_data.csv', delimiter=';')
-
-# Read the second CSV file 
-df2 = pd.read_csv('companies_city.csv')
-
-# Group the company data by city and create an array of JSON objects for each city
-df2_arranged = df2.groupby('city').apply(lambda x: x[['id', 'address', 'zip', 'created_at']].to_dict('records')).reset_index()
-df2_arranged.columns = ['city', 'companies']
-
-# Perform the inner join based on the common 'id' column
-merged_df = pd.merge(df1, df2_arranged, left_on='city', right_on='city', how='inner')
-
-# Rename "column 19" to "country"
-merged_df = merged_df.rename(columns={"column_19": "country", "column_20": "full_location"})
-
-
-print(merged_df.columns.tolist())
-# drop extra column
-
-# Filter the merged data to include only rows where the country is France
-merged_df_france = merged_df[merged_df['country'] == 'France']
+from create_insert_statements import create_listing_table, create_company_table, listing_table_insert, company_table_insert
 
 
 
+def insert_csv_data(csv_file1, csv_file2):
+    
 
-
-# Connect to the PostgreSQL database
-conn = psycopg2.connect(
+    # Establish a connection to the database
+    conn = psycopg2.connect(
     host= DB_HOST,
     database= DB_NAME,
     port = DB_PORT
 )
 
+    cur = conn.cursor()
+    
+    try:
+        # Execute the CREATE TABLE statements
+        cur.execute(create_listing_table)
+        cur.execute(create_company_table)
+        conn.commit()
 
-# drop if exist, create table and insert it in the database
-cur = conn.cursor()
-cur.execute("DROP TABLE IF EXISTS airbnb")
-cur.execute(create_listing_table)
+        
+        # Insert data from CSV file 1 into the listing table
+        with open(csv_file1, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header row
+            cur.executemany(listing_table_insert, reader)
+            conn.commit()
 
-for _, row in merged_df_france.iterrows():
-    last_review = None if pd.isnull(row['last_review']) or row['last_review'] == 'NaN' else datetime.strptime(row['last_review'], "%Y-%m-%d").date()
-    values = (
-        row['id'],
-        row['name'],
-        row['host_id'],
-        row['neighbourhood'],
-        row['room_type'],
-        row['column_10'],
-        row['minimum_nights'],
-        row['number_of_reviews'],
-        last_review,
-        row['reviews_per_month'],
-        row['calculated_host_listings_count'],
-        row['availability_365'],
-        row['updated_date'],
-        row['city'],
-        row['country'],
-        row['coordinates'],
-        row['full_location'],
-        json.dumps(row['companies']) 
-    )
-    cur.execute(listing_table_insert, values)
+        # Insert data from CSV file 2 into the company table
+        with open(csv_file2, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header row
+            cur.executemany(company_table_insert, reader)
+            conn.commit()
 
-conn.commit()
-cur.close()
-conn.close()
+        print("Data inserted successfully!")
+    except (Exception, psycopg2.Error) as error:
+        print("Error inserting data:", error)
+    finally:
+        # Close the database connection
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
